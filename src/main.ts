584182077465917ea91e33f5fd15ffc697933308
ryo <65832968+ryosafadi@users.noreply.github.com>
2024-10-07 19:30:22 -0700

    Added counter for button clicks and text to display click count (step 2)

--- src/main.ts
+++ src/main.ts
@@ -12,3 +12,15 @@ app.append(header);
 const button = document.createElement("button");
 button.innerHTML = '<font size = "20">🥁</font>';
 app.append(button);
+
+let counter: number = 0;
+
+const beats = document.createElement("div");
+beats.innerHTML = `<font size = "6">${counter} Beats</font>`;
+app.append(beats);
+
+//Event Listener to increment counter when button is clicked
+button.addEventListener("click", () => {
+    counter++;
+    beats.innerHTML = `<font size = "6">${counter} Beats</font>`;
+})
