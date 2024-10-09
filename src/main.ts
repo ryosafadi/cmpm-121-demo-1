891a8e8ad1cf0a55e4bd2dbc8869e606273335a3
ryo <65832968+ryosafadi@users.noreply.github.com>
2024-10-09 09:41:14 -0700

    Added purchasable and stackable autoclick upgrade (step 5)

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,14 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-const button = document.createElement("button");
-button.innerHTML = '<font size = "20">🥁</font>';
-app.append(button);
+const beatButton = document.createElement("button");
+beatButton.innerHTML = '<font size = "20">🥁</font>';
+app.append(beatButton);
+
+const autoBeatButton = document.createElement("button");
+autoBeatButton.innerHTML = '<font size = "20">Increase Autobeats</font>';
+app.append(autoBeatButton);
+autoBeatButton.disabled = true;
 
 let counter: number = 0;
 
@@ -20,8 +25,17 @@ beats.innerHTML = `<font size = "6">${counter} Beats</font>`;
 app.append(beats);
 
 //Event Listener to increment counter when button is clicked
-button.addEventListener("click", () => {
+beatButton.addEventListener("click", () => {
     counter++;
+    beats.innerHTML = `<font size = "6">${counter} Beats</font>`;
+});
+
+let growthMult: number = 0;
+
+//Event Listener to increase beat growth rate when button is clicked
+autoBeatButton.addEventListener("click", () => {
+    counter -= 10;
+    growthMult++;
     beats.innerHTML = `<font size = "6">${counter} Beats</font>`;
 });
 
@@ -31,11 +45,18 @@ function updateCounter(currentTime: number) {
     const deltaTime = (currentTime - previousTime) / 1000;
     previousTime = currentTime;
 
-    counter += deltaTime; 
+    counter += growthMult * deltaTime; 
 
     beats.innerHTML = `<font size="6">${Math.floor(counter)} Beats</font>`;
 
     requestAnimationFrame(updateCounter);
 }
 
+function checkVars() {
+    if (counter >= 10) autoBeatButton.disabled = false;
+    else autoBeatButton.disabled = true;
+    requestAnimationFrame(checkVars);
+}
+
+requestAnimationFrame(checkVars);
 requestAnimationFrame(updateCounter);
